ef7303e69fc7880418d124c2abb87ce79322b2c7
zigerkw <zejdhadzic@hotmail.com>
2021-09-15 23:21:46 +0300

    Druga vjezba

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+// This is a single line JavaScript comment
+
+/* document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+*/
+
+function main () {
+    //Only change code below this line
+
+    //Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
