a649d086199fc56f339399f3a748d87417efab14
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 00:06:04 +0300

    peta vjezba

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+
+    var x = 8;
+
+    // Only change code above this line
+    return x;
+}
+
+console.log(main());
+module.exports = main;
