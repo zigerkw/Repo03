24dcc4ff443cc19f892584c0a739028cd1d6c2b1
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 16:18:02 +0300

    Deseta vjezba

---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    
+    var product = numOne * numTwo;
+
+    // Only change code above this line
+    return product; 
+   
+}
+
+console.log(main(15, 3)); //change this line
+module.exports = main;
