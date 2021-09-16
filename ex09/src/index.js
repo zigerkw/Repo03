239e4ed3225a240267bc66a489c4b1812bd9da14
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 16:06:05 +0300

    deveta vjezba

---
+++ ex09/src/index.js
@@ -0,0 +1,9 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    var difference = numOne - numTwo;
+    //Only change doce above this line
+    return difference;
+}
+
+console.log(main(15, 8));
+module.exports = main;
