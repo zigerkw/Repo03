2c9a4b135ffb76e1710ecb0a38f93c2d322937e0
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 15:51:39 +0300

    Osma vjezba

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    
+    var sum = numOne + numTwo;
+
+    // Only change code above this line
+    return sum; 
+   
+}
+
+console.log(main(15, 8)); //change this line
+module.exports = main;
